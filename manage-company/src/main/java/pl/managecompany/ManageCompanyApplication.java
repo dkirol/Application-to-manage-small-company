bdc3a6819eb7b421523b6666cf20cfb147cc775b
fmatusiak <f.matusiak95@gmail.com>
2019-09-23 22:09:26 +0200

    create new module manage-company in java , create new project front in angular , create packages to existing project

---
+++ manage-company/src/main/java/pl/managecompany/ManageCompanyApplication.java
@@ -0,0 +1,13 @@
+package pl.managecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageCompanyApplication.class, args);
+    }
+
+}
